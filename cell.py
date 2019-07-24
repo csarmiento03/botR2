26a7a8c9e160c90da84de5a276ffc05d6fbbef7c
nathabmmb <nathabmmb@gmail.com>
2019-07-23 23:18:00 -0300

    Corregidos errores sintacticos en cell.py Reemplazados tabs por espacios en maze.py

--- cell.py
+++ cell.py
@@ -4,66 +4,92 @@
 
 
 class Cell(object):
-    """Definicion del constructor de la clase donde escribo todos los 
-    atributos o variables que va a tener la clase y que pueden 
-    ser llamados por los otros objeto"""
-    def __init__(self, pos, walls):
+
+    """
+    Clase que conforma laberintos con atributos y métodos que facilitan su construcción.
+    """
+
+    def __init__(self, pos, walls=False):
+        """
+        Definicion del constructor de la clase donde escribo todos los 
+        atributos o variables que va a tener la clase y que pueden 
+        ser llamados por los otros objeto.
+        """
         self.pos = pos         
-        self.star = True
-        self.end = True
+        self.start = False
+        self.end = False
         self.visited = False
-        self.wallN = True
-        self.wallS = True
-        self.wallE = True
-        self.wallW = True
-    
-    """setter y getter para iniciar y finzalizar la construcción del
-    laberinto."""
+        if walls:
+            self.wallN = True
+            self.wallS = True
+            self.wallE = True
+            self.wallW = True
+        else:
+            self.wallN = False
+            self.wallS = False
+            self.wallE = False
+            self.wallW = False
+
+    """ Setters y getters para respetar el paradigma OOP."""
     def setStart(self, arg):
         self.start = arg
-    def getStar():
-        return self.star()
-        
+
+    def getStart():
+        return self.start()
+	    
     def setEnd(self, arg):
         self.end = arg
+
     def getEnd():
         return self.end()
-    
-    def getWalls(self):
-        pass
-        
+
     def setVisited(self, arg):
-        self.visited =arg
+        self.visited = arg
+
     def getVisited(self, arg):
         return self.visited()
-    
-    """Metodo encontrar vecinos en el cual, primero se calculan 
-    los primeros vecinos respecto a su posicion actual. Luego, se
-    revisa si esos vecinos hacen parte de la grilla y si han sido
-    o no visitados. Para esto, se usa el atributo visited"""    
-    def findNeighbours(self, grid):
-        
-        auxList = []
-        try: auxList.append(grid[self.pos[0]+1][self.pos[1]])
-        except: pass
-        try: auxList.append(grid[self.pos[0]-1][self.pos[1]])
-        except: pass
-        try: auxList.append(grid[self.pos[1]+1][self.pos[0]])
-        except: pass
-        try: auxList.append(grid[self.pos[1]-1][self.pos[0]])
-        except: pass
 
-        for i in auxList:
-            if i.visited == True:
-                auxList.pop(i)
-    """Metodo que hace una revision de la ubicacion de la otra
-    celda con respecto a la celda en la cual se encuentra.
-    """    
+    def getWalls(self):
+        return [self.wallW, self.wallN, self.wallE, self.wallS]
+        
+    def findNeighbours(self, grid):
+        """
+        Metodo encontrar vecinos en el cual, primero se calculan 
+        los primeros vecinos respecto a su posicion actual. Luego, se
+        revisa si esos vecinos hacen parte de la grilla y si han sido
+        o no visitados. Para esto, se usa el atributo visited.
+        """    
+
+        # neighbours = []
+        # try: neighbours.append(grid[self.pos[0]+1][self.pos[1]])
+        # except: pass
+        # try: neighbours.append(grid[self.pos[0]-1][self.pos[1]])
+        # except: pass
+        # try: neighbours.append(grid[self.pos[1]+1][self.pos[0]])
+        # except: pass
+        # try: neighbours.append(grid[self.pos[1]-1][self.pos[0]])
+        # except: pass
+
+        # Lo de arriba, pero reimplementado en menos lineas y corregido error en los índices!
+        tries = [(self.pos[0]+1, self.pos[1]), (self.pos[0]-1, self.pos[1]),
+                (self.pos[0], self.pos[1]+1), (self.pos[0], self.pos[1]-1)]
+        for t in tries:
+            try: neighbours.append(grid[t[0]][t[1]])
+            except: pass
+
+        for i in neighbours:
+            if i.visited:
+                neighbours.pop(i)
+ 
     def checkWall(self, otherCell):
+        """
+        Metodo que hace una revision de la ubicacion de la otra
+        celda con respecto a la celda en la cual se encuentra.
+        """   
         x0 = self.pos[1]
         y0 = self.pos[0]
         x1 = otherCell.pos[1]
-        x1 = otherCell.pos[0]
+        y1 = otherCell.pos[0]
         dx = x0-x1
         dy = y0-y1
         if dx==1 and dy==0 and wallN:
@@ -76,10 +102,12 @@ class Cell(object):
             return True
         else:
             return False
-    """Metodo para destruir la pared de la celda vecina.
-    Aquí rompe su propia pared y la del otro.
-    """
+
     def destroyWall():
+        """
+        Metodo para destruir la pared de la celda vecina.
+        Aquí rompe su propia pared y la del otro.
+        """
         x0 = self.pos[1]
         y0 = self.pos[0]
         x1 = otherCell.pos[1]
--- maze.py
+++ maze.py
@@ -4,118 +4,120 @@
 
 import numpy as np
 import random
-import cell
+from cell import Cell
 
 
 
 class Maze():
-	"""
-	Esta versión genérica solo construlle un laberinto trivial 
-	que consiste en una caja con una entrada y una salida.
+    """
+    Esta versión genérica solo construlle un laberinto trivial 
+    que consiste en una caja con una entrada y una salida.
 
-	"""
+    """
 
-	def __init__(self):
-		self.grid = None
+    def __init__(self):
+        self.grid = None
 
-	def saveMaze(self, fname):
-		pass
+    def saveMaze(self, fname):
+        pass
 
-	def initMaze(self, size, walls=True):
-		"""
-		Crea la grid de dimensiones dadas por "size".
-		"walls" indica si las celdas están rodeadas de paredes por defecto.
-		"""
-		self.grid = list()
-		for i in range(size[0]):
-			row = list()
-			for j in range(size[1]):
-				row.append(Cell(pos=(i,j), walls=walls))
-			self.grid.append(row)
+    def initMaze(self, size, walls=True):
+        """
+        Crea la grid de dimensiones dadas por "size".
+        "walls" indica si las celdas están rodeadas de paredes por defecto.
+        """
+        self.grid = list()
+        for i in range(size[0]):
+            row = list()
+            for j in range(size[1]):
+                row.append(Cell(pos=(i,j), walls=walls))
+            self.grid.append(row)
 
-	def buildMaze(self,size, start=False, end=False):
-		"""
-		Función dummy
-		"""
-		pass
+    def buildMaze(self,size, start=False, end=False):
+        """
+        Función dummy
+        """
+        pass
 
 class BoxMaze(Maze):
 
-	def buildMaze(self, size, start=False, end=False):
-		"""
-		Crea un laberinto trivial de tamaño "size" que consiste en
-		una caja vacía. "start" y "end" son tuplas que pueden especificar
-		la posición de la entrada y la salida del laberinto.
-		"""
-		# Asigna entrada y salida por default. Define variables auxiliares.
-		bottom = size[0] -1
-		right = size[1] -1
-		if not start: start = (0,0)
-		if not end: end = (bottom, right)
+    def buildMaze(self, size, start=False, end=False):
+        """
+        Crea un laberinto trivial de tamaño "size" que consiste en
+        una caja vacía. "start" y "end" son tuplas que pueden especificar
+        la posición de la entrada y la salida del laberinto.
+        """
+        # Asigna entrada y salida por default. Define variables auxiliares.
+        bottom = size[0] -1
+        right = size[1] -1
+        if not start: start = (0,0)
+        if not end: end = (bottom, right)
 
 
-		# Implementa un laberinto con forma de caja
-		for i in range(size[0]):
-			for j in range(size[1]):
-				if j != right:
-					self.grid[i][j].wallE = False
-				if j != 0: 
-					self.grid[i][j].wallW = False
-				if i != bottom:
-					self.grid[i][j].wallS = False
-				if i != 0:
-					self.grid[i][j].wallN = False
+        # Implementa un laberinto con forma de caja
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if j != right:
+                    self.grid[i][j].wallE = False
+                if j != 0: 
+                    self.grid[i][j].wallW = False
+                if i != bottom:
+                    self.grid[i][j].wallS = False
+                if i != 0:
+                    self.grid[i][j].wallN = False
 
-		#Implementa la entrada y salida.
-		self.grid[start[0]][start[1]].wallN = False
-		self.grid[start[0]][start[1]].start = True
-		self.grid[end[0]][end[1]].wallS = False
-		self.grid[end[0]][end[1]].end = True
+        #Implementa la entrada y salida.
+        self.grid[start[0]][start[1]].wallN = False
+        self.grid[start[0]][start[1]].start = True
+        self.grid[end[0]][end[1]].wallS = False
+        self.grid[end[0]][end[1]].end = True
 
 
 class BacktrackingMaze(Maze):
-	"""
-	Este laberinto implementa el algoritmo de recursive backtracking.
-	Es un algoritmo sencillo que favorece corredores largos.
-	No es muy eficiente en términos de memoria, pero funciona bien para
-	laberintos no gigantes :)
-	"""
+    """
+    Este laberinto implementa el algoritmo de recursive backtracking.
+    Es un algoritmo sencillo que favorece corredores largos.
+    No es muy eficiente en términos de memoria, pero funciona bien para
+    laberintos no gigantes :)
+    """
 
-	def buildMaze(self,size,start=False,end=False):
-		"""
-		Construye el laberinto de acuerdo al siguiente algoritmo:
-		Inicio: 
-			-Elige una celda al azar y la añade al stack
-		Recursivo:
-			-Elige un vecino al azar, revisa que no haya sido visitado
-			-Destruye paredes entre la celda actual y la seleccionada
-			-Añade la celda seleccionada al stack y continua
-			-Si la celda nueva ya fue visitada, se hace backtracking
-		Fin:
-			-No existen celdas que no hayan sido visitadas
+    def buildMaze(self,size,start=False,end=False):
+        """
+        Construye el laberinto de acuerdo al siguiente algoritmo:
+        Inicio: 
+            -Elige una celda al azar y la añade al stack
+        Recursivo:
+            -Elige un vecino al azar, revisa que no haya sido visitado
+            -Destruye paredes entre la celda actual y la seleccionada
+            -Añade la celda seleccionada al stack y continua
+            -Si la celda nueva ya fue visitada, se hace backtracking
+        Fin:
+            -No existen celdas que no hayan sido visitadas
 
-		"start" y "end" son tuplas que pueden especificar
-		la posición de la entrada y la salida del laberinto.
-		"""
-		stack = []
-		cell = grid[random.randint(0,bottom)][random.randint[0,right]]
-		cell.setVisited(True)
-		stack.append(cell)
-		while len(stack) > 0:
-			try: 
-				newcell = chooseNew(self,cell)
-				if not newcell.getVisited():
-					cell.destroyWall(newcell)
-					newcell.setVisited(True)
-					stack.append(newcell)
-					continue
-				else: cell = stack.pop()
+        "start" y "end" son tuplas que pueden especificar
+        la posición de la entrada y la salida del laberinto.
+        """
+        stack = []
+        cell = grid[random.randint(0,bottom)][random.randint[0,right]]
+        cell.setVisited(True)
+        stack.append(cell)
+        while len(stack) > 0:
+            try: 
+                newcell = chooseNew(self,cell)
+                if not newcell.getVisited():
+                    cell.destroyWall(newcell)
+                    newcell.setVisited(True)
+                    stack.append(newcell)
+                    continue
+                else: 
+                    cell = stack.pop()
+            except: pass
 
-	def chooseNew(self,cell):
-		"""
-		Elige una nueva celda vecina al azar y la devuelve.
-		"""
-		pass
+    def chooseNew(self,cell):
+        """
+        Elige una nueva celda vecina al azar y la devuelve.
+        """
+        pass
 
 
 
@@ -123,7 +125,7 @@ class BacktrackingMaze(Maze):
 
 
 if __name__ == "__main__":
-	maze = BoxMaze()
-	size = (3,3)
-	maze.initMaze(size)
-	maze.buildMaze(size)
+    maze = BoxMaze()
+    size = (3,3)
+    maze.initMaze(size)
+    maze.buildMaze(size)
